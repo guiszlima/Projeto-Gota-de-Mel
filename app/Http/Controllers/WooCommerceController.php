20b12ce11c191fc6b29a1936aa0f907fe1537b80
guilhermespicacci <guilhermespicaccilima@gmail.com>
2024-06-19 01:39:36 -0300

    Development and testing stock system

--- app/Http/Controllers/WooCommerceController.php
+++ app/Http/Controllers/WooCommerceController.php
@@ -29,7 +29,35 @@ class WooCommerceController extends Controller
     {
         try {
             $products = $this->woocommerce->get('products');
-            return response()->json($products); // Retorna os produtos como JSON
+            
+            dd($products[0]->stock_quantity); // Retorna os produtos como JSON
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+
+    public function sellProducts($id){
+        $product = $this->woocommerce->get("products/{$id}");
+
+
+        return view("edit_product")
+        ->with("product",$product);
+    }
+
+    public function updateProducts(Request $request){
+        try {
+            $id = $request->id_from_request;
+            $products = $this->woocommerce->get("products/{$id}");
+            $before_sell =   $products->stock_quantity;
+            $after_sell = $before_sell-1;
+            $data = [
+                'stock_quantity' => $after_sell
+            ];
+            
+            $update = $this->woocommerce->put("products/{$id}", $data);
+        echo 'Feito Corretamente' ;
+
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ProductController;
+
 
 use App\Http\Controllers\WooCommerceController;
 
-Route::get('/get-products', [WooCommerceController::class, 'getProducts']);
+Route::get('/get-products', [WooCommerceController::class, 'getProducts'])->name('get.products');
+Route::get('sell-product/{id}',[WooCommerceController::class,'sellProducts'])->name('sell.products');
+Route::put('update-product',[WooCommerceController::class,'updateProducts'])->name('update.products');
