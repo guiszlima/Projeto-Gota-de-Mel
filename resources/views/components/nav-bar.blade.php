7ac809645b943c5c374f300020301903633813c0
guilhermespicacci <guilhermespicaccilima@gmail.com>
2024-07-06 03:49:02 -0300

    ajuste no header

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,15 +1,16 @@
-
-
 <header class="bg-gray-800 text-white shadow">
-        <div class="container mx-auto flex justify-between items-center py-4 px-6">
-            <div class="text-lg font-semibold">
-                <a href="#" class="hover:text-gray-300">My Logo</a>
-            </div>
-            <nav class="flex space-x-4">
-                <a href="#" class="hover:text-gray-300">Home</a>
-                <a href="#" class="hover:text-gray-300">About</a>
-                <a href="#" class="hover:text-gray-300">Services</a>
-                <a href="#" class="hover:text-gray-300">Contact</a>
-            </nav>
+    <div class="container mx-auto flex justify-between items-center py-4 px-6">
+        <div class="text-lg font-semibold">
+            <a href="#" class="hover:text-gray-300">My Logo</a>
         </div>
+        <nav class="flex space-x-4">
+            <a href="#" class="hover:text-gray-300">Home</a>
+            <a href="#" class="hover:text-gray-300">About</a>
+            <a href="#" class="hover:text-gray-300">Services</a>
+            <a href="#" class="hover:text-gray-300">Contact</a>
+        </nav>
+        <form action="{{ route('logout') }}" method="post">
+            <button type="submit" class="border-2 border-red-600 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200">Sair</button>
+        </form>
+    </div>
 </header>
