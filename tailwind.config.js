82ffb40aaa964f0ac70a9023f355472a16b1e9e6
guiszlima <gui.spicacci.dev@gmail.com>
2025-05-08 01:49:01 -0300

    Add safelist styles to tailwind configuration for improved utility usage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    safelist: [
+        "h-[400px]", // ✅ evita que seja removida na produção
+        "overflow-y-auto",
+        "flex",
+        "flex-wrap",
+        "gap-6",
+        "justify-start",
+    ],
     theme: {
         extend: {
             screens: {
